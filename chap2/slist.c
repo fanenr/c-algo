007e3fa0523c08ea069779ad4c65648fef88c429
Arthur63 <arthur63@163.com>
2023-11-12 22:28:04 +0800

    Update chap2

--- chap2/slist.c
+++ chap2/slist.c
@@ -1,1 +1,19 @@
 #include "slist.h"
+#include <stdlib.h>
+
+slist *slist_init(slist *list)
+{
+    list->size = 0;
+    list->head = NULL;
+    return list;
+}
+
+void slist_free(slist *list)
+{
+    struct slist_n *node = list->head;
+    while (node != NULL) {
+        free(node);
+        node = node->next;
+    }
+    slist_init(list);
+}
