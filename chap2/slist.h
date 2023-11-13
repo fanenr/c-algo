e7823b418e498727a2cfc504e75aeb6eb2aeac5b
Arthur63 <arthur63@163.com>
2023-11-13 19:53:55 +0800

    Update chap2

--- chap2/slist.h
+++ chap2/slist.h
@@ -34,18 +34,24 @@ extern slist *slist_init(slist *list);
 extern void slist_free(slist *list);
 
 /*
+ * malloc a node
+ */
+extern struct slist_n *slist_node(void);
+
+/*
  * insert a `node` after `pos` in the `list`
  * if the `pos` is NULL, the `node` will be inserted at the beginning of the
  * `list` insert will change `.size` of the `list` if `node` has been inserted
  */
-extern slist *slist_insert(slist *restrict list, struct slist_n *restrict pos,
-                           struct slist_n *restrict node);
+extern struct slist_n *slist_insert(slist *restrict list,
+                                    struct slist_n *restrict pos,
+                                    struct slist_n *restrict node);
 
 /*
  * remove a node at `pos` in the `list`
  * no node will be removed if the `pos` is NULL
  * remove will change `.size` of the `list if `pos` has been removed
  */
-extern slist *slist_remove(slist *restrict list, struct slist_n *restrict pos);
+extern void slist_remove(slist *restrict list, struct slist_n *restrict pos);
 
 #endif
---
+++ chap2/tests/slist.c
@@ -0,0 +1,33 @@
+#include "../slist.h"
+#include <assert.h>
+#include <stdlib.h>
+
+int main()
+{
+    slist list;
+
+    slist_init(&list);
+    assert(list.head == NULL && list.size == 0);
+
+    int val = 10;
+    struct slist_n *node = slist_node();
+    node->value = &val;
+    assert(slist_insert(&list, NULL, node) == node);
+    assert(list.head == node && list.size == 1);
+    assert(list.head->value == &val);
+
+    int *val2 = malloc(sizeof(int));
+    struct slist_n *node2 = slist_node();
+    node2->value = val2;
+    assert(slist_insert(&list, node, node2) == node2);
+    assert(list.head == node && list.size == 2);
+    assert(list.head->next == node2);
+
+    slist_remove(&list, node);
+    assert(list.head == node2 && list.size == 1);
+
+    slist_remove(&list, node2);
+    assert(list.head == NULL && list.size == 0);
+    
+    slist_free(&list);
+}
