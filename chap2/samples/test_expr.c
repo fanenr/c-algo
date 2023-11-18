846ce518e1631261fea6eaa1114c92e41e5bee69
Arthur63 <Arthur63@163.com>
2023-11-18 20:40:08 +0800

    Update chap2: basically finish expr

--- chap2/samples/test_expr.c
+++ chap2/samples/test_expr.c
@@ -1,4 +1,8 @@
 #include "expr.h"
+#include <assert.h>
+#include <stdio.h>
+
+#define BUFF_SIZE 1024
 
 static void test_in2post();
 
@@ -7,4 +11,29 @@ int main(void)
     test_in2post();
 }
 
-void test_in2post() { }
+void test_in2post()
+{
+    slist list;
+    char buf[BUFF_SIZE];
+
+    scanf("%s", buf);
+    list = in2post(buf, BUFF_SIZE);
+
+    assert(list.size != 0);
+
+    struct slist_n *node;
+    struct expr_node *enode;
+
+    for (node = list.head; node != NULL; node = node->next) {
+        enode = node->data.ptr;
+        if (enode->type == EXPR_NODE_FLOAT) {
+            printf("%.2lf ", enode->data.floating);
+        } else if (enode->type == EXPR_NODE_INT) {
+            printf("%ld ", enode->data.integer);
+        } else {
+            printf("%c ", (char)enode->type);
+        }
+    }
+
+    expr_slist_free(&list);
+}
