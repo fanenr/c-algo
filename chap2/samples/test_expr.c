4b57593a4ea9e38c75b7ed0ab246b1bf989b87e0
Arthur63 <arthur63@163.com>
2023-11-19 18:19:20 +0800

    Update chap2: add expr_evaluate

--- chap2/samples/test_expr.c
+++ chap2/samples/test_expr.c
@@ -17,14 +17,14 @@ void test_in2post()
     char buf[BUFF_SIZE];
 
     struct slist_n *node;
-    struct expr_node *enode;
+    struct expr_node *enode, result;
 
     while (fgets(buf, BUFF_SIZE, stdin) != NULL) {
         printf("infix: %s", buf);
-        list = in2post(buf, BUFF_SIZE);
+        list = expr_in2post(buf, BUFF_SIZE);
         assert(list.size != 0);
         printf("postfix: ");
-        
+
         for (node = list.head; node != NULL; node = node->next) {
             enode = node->data.ptr;
             if (enode->type == EXPR_NODE_FLOAT) {
@@ -36,7 +36,14 @@ void test_in2post()
             }
         }
 
+        expr_evaluate(&list, &result);
+        assert(result.type != EXPR_NODE_NONE);
+        if (result.type == EXPR_NODE_INT)
+            printf("= %ld", result.data.integer);
+        else
+            printf("= %.2lf", result.data.floating);
+
         printf("\n\n");
-        expr_slist_free(&list);
+        expr_free(&list);
     }
 }
