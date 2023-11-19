58e88e59f72e9b3219f34d4de114e0c90ec1545c
Arthur63 <arthur63@163.com>
2023-11-19 11:17:14 +0800

    Update chap2: add symbol bit detection for expr

--- chap2/samples/expr.c
+++ chap2/samples/expr.c
@@ -17,13 +17,7 @@ static struct expr_node *alloc_expr_node()
     return node;
 }
 
-#define save_expr_node(plist, enode, lnode, ltail)                             \
-    enode = alloc_expr_node();                                                 \
-    lnode = slist_node();                                                      \
-    lnode->data.ptr = enode;                                                   \
-    ltail = slist_insert(plist, ltail, lnode);
-
-slist in2post(char *src, size_t len)
+slist in2post(const char *src, size_t len)
 {
     stack stac;
     slist list;
@@ -33,32 +27,47 @@ slist in2post(char *src, size_t len)
     struct stack_n stac_node, *stac_top;
     struct slist_n *list_node, *list_tail;
     list_tail = NULL;
-    stac_node.data.u8 = 0;
 
     long tmpi;
     double tmpf;
     char *endi, *endf;
     struct expr_node *enode;
 
+#define LAST_READ_START 0
+#define LAST_READ_SYMBOL 1
+#define LAST_READ_NUMBER 2
+#define LAST_READ_BRACKET 3
+#define LAST_READ_OPERATOR 4
+
+    int symbol_bit = 1;
+    int last_read = LAST_READ_START;
+
+#define save_expr_node(plist, enode, lnode, ltail)                             \
+    enode = alloc_expr_node();                                                 \
+    lnode = slist_node();                                                      \
+    lnode->data.ptr = enode;                                                   \
+    ltail = slist_insert(plist, ltail, lnode);
+
     for (int i = 0; i < len; i++) {
         switch (src[i]) {
         case '0' ... '9':
             tmpf = strtod(src + i, &endf);
             tmpi = strtol(src + i, &endi, 10);
-
             save_expr_node(&list, enode, list_node, list_tail);
 
             if (endf != src + i && *endi == '.') { /* got a floating */
-                enode->data.floating = tmpf;
+                enode->data.floating =
+                    last_read == LAST_READ_SYMBOL ? tmpf * symbol_bit : tmpf;
                 enode->type = EXPR_NODE_FLOAT;
                 i = endf - src - 1;
             } else if (endi != src + i) { /* got an integer */
-                enode->data.integer = tmpi;
+                enode->data.integer =
+                    last_read == LAST_READ_SYMBOL ? tmpi * symbol_bit : tmpf;
                 enode->type = EXPR_NODE_INT;
                 i = endi - src - 1;
-            } else {
-                goto err;
             }
+
+            last_read = LAST_READ_NUMBER;
             break;
 
         case '+':
@@ -73,20 +82,32 @@ slist in2post(char *src, size_t len)
                     break;
 
                 stac_node = stack_pop(&stac);
-
                 save_expr_node(&list, enode, list_node, list_tail);
-
                 enode->type = (enum EXPR_NODE_TYPE)stac_node.data.u8;
             }
+
             stac_node.data.u8 = src[i];
-            stack_push(&stac, stac_node);
+
+            /* check symbol bit */
+            if (last_read == LAST_READ_START ||
+                last_read == LAST_READ_BRACKET) {
+                symbol_bit = src[i] == '+' ? 1 : -1;
+                last_read = LAST_READ_SYMBOL;
+            } else {
+                stack_push(&stac, stac_node);
+                last_read = LAST_READ_OPERATOR;
+            }
             break;
 
         case '*':
         case '/':
+            /* check invalid symbol */
+            if (last_read == LAST_READ_START || last_read == LAST_READ_BRACKET)
+                goto err;
         case '(':
             stac_node.data.u8 = src[i];
             stack_push(&stac, stac_node);
+            last_read = src[i] == '(' ? LAST_READ_BRACKET : LAST_READ_OPERATOR;
             break;
 
         case ')':
@@ -96,11 +117,13 @@ slist in2post(char *src, size_t len)
                     break;
 
                 save_expr_node(&list, enode, list_node, list_tail);
-
                 enode->type = (enum EXPR_NODE_TYPE)stac_node.data.u8;
             }
             if (stac_node.data.u8 != '(')
                 goto err;
+            break;
+
+        case ' ':
             break;
 
         case '\0':
@@ -111,16 +134,22 @@ slist in2post(char *src, size_t len)
         }
     }
 
+#undef LAST_READ_START
+#undef LAST_READ_SYMBOL
+#undef LAST_READ_NUMBER
+#undef LAST_READ_BRACKET
+#undef LAST_READ_OPERATOR
+
 end:
     while (stac.size != 0) {
         stac_node = stack_pop(&stac);
-
         save_expr_node(&list, enode, list_node, list_tail);
-
         enode->type = (enum EXPR_NODE_TYPE)stac_node.data.u8;
     }
     stack_free(&stac);
     return list;
+
+#undef save_expr_node
 
 err:
     stack_free(&stac);
