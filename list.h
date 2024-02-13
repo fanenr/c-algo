90f3ad9ba6a0f1115ee9a48945beda50c5809de9
Arthur63 <Arthur63@163.com>
2024-02-13 18:58:26 +0800

    refactor list

--- list.h
+++ list.h
@@ -4,13 +4,19 @@
 #include <stdalign.h>
 #include <stddef.h>
 
+typedef struct list_i
+{
+  size_t size;
+  size_t align;
+} list_i;
+
 typedef struct list_n list_n;
 
 struct list_n
 {
   list_n *prev;
   list_n *next;
-  max_align_t data[];
+  void *data;
 };
 
 typedef struct list
@@ -24,71 +30,84 @@ extern void list_init (list *lis) __attribute__ ((nonnull (1)));
 
 extern void list_free (list *lis) __attribute__ ((nonnull (1)));
 
-extern list_n *list_at (list *lis, size_t pos) __attribute__ ((nonnull (1)));
-
-extern list_n *list_remove (list *restrict lis, list_n *restrict pos)
+extern void list_remove (list *lis, list_n *pos)
     __attribute__ ((nonnull (1, 2)));
 
-extern list_n *list_push_back (list *restrict lis, void *restrict data,
-                               size_t ele) __attribute__ ((nonnull (1, 2)));
+extern list_n *list_at (list *lis, size_t pos) __attribute__ ((nonnull (1)));
 
-extern list_n *list_push_front (list *restrict lis, void *restrict data,
-                                size_t ele) __attribute__ ((nonnull (1, 2)));
-
-extern list_n *list_insert (list *restrict lis, list_n *restrict pos,
-                            void *restrict data, size_t ele)
+extern list_n *list_push_back (list *lis, void *data, const list_i *info)
     __attribute__ ((nonnull (1, 2, 3)));
 
-#define LIST_DEF_TYPE(TYPE, PRE)                                              \
+extern list_n *list_push_front (list *lis, void *data, const list_i *info)
+    __attribute__ ((nonnull (1, 2, 3)));
+
+extern list_n *list_insert (list *lis, list_n *pos, void *data,
+                            const list_i *info)
+    __attribute__ ((nonnull (1, 2, 3, 4)));
+
+#define LIST_DEF_INFO(TYPE, PRE)                                              \
   typedef struct PRE##_list_n PRE##_list_n;                                   \
+                                                                              \
   struct PRE##_list_n                                                         \
   {                                                                           \
     PRE##_list_n *prev;                                                       \
     PRE##_list_n *next;                                                       \
-    alignas (max_align_t) TYPE data[];                                        \
-  };
+    TYPE *data;                                                               \
+  };                                                                          \
+                                                                              \
+  static const list_i PRE##_list_info                                         \
+      = { .size = sizeof (TYPE), .align = alignof (TYPE) };                   \
+                                                                              \
+  static void PRE##_list_remove (list *lis, PRE##_list_n *pos)                \
+      __attribute__ ((nonnull (1, 2)));                                       \
+                                                                              \
+  static PRE##_list_n *PRE##_list_at (list *lis, size_t pos)                  \
+      __attribute__ ((nonnull (1)));                                          \
+                                                                              \
+  static inline PRE##_list_n *PRE##_list_push_back (list *lis, TYPE data)     \
+      __attribute__ ((nonnull (1)));                                          \
+                                                                              \
+  static inline PRE##_list_n *PRE##_list_push_front (list *lis, TYPE data)    \
+      __attribute__ ((nonnull (1)));                                          \
+                                                                              \
+  static inline PRE##_list_n *PRE##_list_insert (                             \
+      list *lis, PRE##_list_n *pos, TYPE data)                                \
+      __attribute__ ((nonnull (1, 2)));
 
 #define LIST_DEF_AT(TYPE, PRE)                                                \
-  __attribute__ ((nonnull (1))) static inline PRE##_list_n *PRE##_list_at (   \
-      list *lis, size_t pos)                                                  \
+  static inline PRE##_list_n *PRE##_list_at (list *lis, size_t pos)           \
   {                                                                           \
     return (PRE##_list_n *)list_at (lis, pos);                                \
   }
 
 #define LIST_DEF_REMOVE(TYPE, PRE)                                            \
-  __attribute__ ((nonnull (1, 2))) static inline PRE##_list_n                 \
-      *PRE##_list_remove (list *restrict lis, PRE##_list_n *restrict pos)     \
+  static inline void PRE##_list_remove (list *lis, PRE##_list_n *pos)         \
   {                                                                           \
-    return (PRE##_list_n *)list_remove (lis, (list_n *)pos);                  \
+    list_remove (lis, (list_n *)pos);                                         \
   }
 
 #define LIST_DEF_PUSH_BACK(TYPE, PRE)                                         \
-  __attribute__ ((nonnull (                                                   \
-      1))) static inline PRE##_list_n *PRE##_list_push_back (list *lis,       \
-                                                             TYPE data)       \
+  static inline PRE##_list_n *PRE##_list_push_back (list *lis, TYPE data)     \
   {                                                                           \
-    return (PRE##_list_n *)list_push_back (lis, &data, sizeof (TYPE));        \
+    return (PRE##_list_n *)list_push_back (lis, &data, &PRE##_list_info);     \
   }
 
 #define LIST_DEF_PUSH_FRONT(TYPE, PRE)                                        \
-  __attribute__ ((nonnull (                                                   \
-      1))) static inline PRE##_list_n *PRE##_list_push_front (list *lis,      \
-                                                              TYPE data)      \
+  static inline PRE##_list_n *PRE##_list_push_front (list *lis, TYPE data)    \
   {                                                                           \
-    return (PRE##_list_n *)list_push_front (lis, &data, sizeof (TYPE));       \
+    return (PRE##_list_n *)list_push_front (lis, &data, &PRE##_list_info);    \
   }
 
 #define LIST_DEF_INSERT(TYPE, PRE)                                            \
-  __attribute__ ((nonnull (1, 2))) static inline PRE##_list_n                 \
-      *PRE##_list_insert (list *restrict lis, PRE##_list_n *restrict pos,     \
-                          TYPE data)                                          \
+  static inline PRE##_list_n *PRE##_list_insert (                             \
+      list *lis, PRE##_list_n *pos, TYPE data)                                \
   {                                                                           \
     return (PRE##_list_n *)list_insert (lis, (list_n *)pos, &data,            \
-                                        sizeof (TYPE));                       \
+                                        &PRE##_list_info);                    \
   }
 
 #define LIST_DEF_ALL(TYPE, PRE)                                               \
-  LIST_DEF_TYPE (TYPE, PRE);                                                  \
+  LIST_DEF_INFO (TYPE, PRE);                                                  \
   LIST_DEF_AT (TYPE, PRE);                                                    \
   LIST_DEF_REMOVE (TYPE, PRE);                                                \
   LIST_DEF_PUSH_BACK (TYPE, PRE);                                             \
