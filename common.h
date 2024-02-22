8c8d79058423d65fd0b62bc5672842f203fdeadb
Arthur63 <Arthur63@163.com>
2024-02-22 19:59:51 +0800

    update tests for avlmap

--- common.h
+++ common.h
@@ -1,10 +1,39 @@
 #ifndef TIME_H
 #define TIME_H
 
+#include <stdlib.h>
 #include <time.h>
 
 #define TIME_ST() clock_t __tm_st__ = clock ();
 #define TIME_ED() clock_t __tm_ed__ = clock ();
 #define TIME_VAL() ((double)(__tm_ed__ - __tm_st__) / CLOCKS_PER_SEC)
+
+static void rand_init (void) __attribute__ ((constructor));
+static long rand_long (long from, long to);
+static char *rand_string (size_t len);
+
+static inline void
+rand_init (void)
+{
+  srand ((unsigned)time (NULL));
+}
+
+static inline long
+rand_long (long from, long to)
+{
+  return rand () % (to - from) + from;
+}
+
+static inline char *
+rand_string (size_t len)
+{
+  char *ret = malloc (sizeof (char) * (len + 1));
+  if (!ret)
+    return NULL;
+  for (size_t i = 0; i < len; i++)
+    ret[i] = (char)rand_long (32, 127);
+  ret[len] = '\0';
+  return ret;
+}
 
 #endif
--- tests/avlmap.c
+++ tests/avlmap.c
@@ -1,6 +1,14 @@
 #include "../avlmap.h"
+#include "../common.h"
 #include <assert.h>
+#include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
+
+#define N 100000
+
+char *names[N];
+int ages[N];
 
 int
 si_avlmap_comp (const char **key1, const char **key2)
@@ -18,13 +26,43 @@ main (void)
   avlmap map;
   avlmap_init (&map);
 
-  si_avlmap_insert (&map, "Tom", 18);
-  si_avlmap_insert (&map, "Jack", 19);
-  si_avlmap_insert (&map, "Arthur", 20);
+  for (size_t i = 0; i < N; i++)
+    {
+      char *name = rand_string (rand_long (8, 17));
+      assert (name);
+      int age = rand_long (1, 101);
+      names[i] = name;
+      ages[i] = age;
+      si_avlmap_n *node = si_avlmap_insert (&map, name, age);
+      if (!node)
+        names[i] = NULL;
+    }
 
-  assert (si_avlmap_find (&map, "Tom")->val == 18);
-  assert (si_avlmap_find (&map, "Jack")->val == 19);
-  assert (si_avlmap_find (&map, "Arthur")->val == 20);
+  /* printf ("len: %lu\n", map.len); */
 
+  for (size_t i = 0; i < N / 4; i++)
+    {
+      long rmpos = rand_long (0, N);
+      if (!names[rmpos])
+        continue;
+      size_t len = map.len;
+      si_avlmap_remove (&map, names[rmpos]);
+      assert (map.len != len);
+      free (names[rmpos]);
+      names[rmpos] = NULL;
+    }
+
+  /* printf ("len: %lu\n", map.len); */
+
+  for (size_t i = 0; i < N; i++)
+    {
+      if (!names[i])
+        continue;
+      si_avlmap_n *node = si_avlmap_find (&map, names[i]);
+      assert (node->val == ages[i]);
+    }
+
+  for (size_t i = 0; i < N; i++)
+    free (names[i]);
   avlmap_free (&map);
 }
