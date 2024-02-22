d5c17c32d1f60f5881ab19ecb025f961f5109ce7
Arthur63 <arthur63@163.com>
2024-02-22 20:52:31 +0800

    fix some bugs in hashmap

--- hashmap.c
+++ hashmap.c
@@ -2,7 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-#define NEXT_INDEX(INDEX, TIME) ((INDEX) + (TIME) * (TIME))
+#define INDEX_AT(MAP, HASH) ((HASH) % (MAP)->cap)
+#define INDEX_NEXT(INDEX, TIME) ((INDEX) + (TIME) * (TIME))
 #define NODE_AT(MAP, INDEX, INFO) ((MAP)->data + (INDEX) * (INFO)->n_size)
 
 #define STS_OF(NODE) ((char *)NODE)
@@ -28,39 +29,32 @@ hashmap_reserve (hashmap *map, size_t cap, const hashmap_i *info)
   if (map->cap >= cap)
     return map;
 
-  size_t ncap = map->cap;
-  if (ncap < HASHMAP_INIT_CAP)
-    ncap = HASHMAP_INIT_CAP;
-
-  while (ncap < cap)
-    ncap *= HASHMAP_EXPAN_RATIO;
-
-  void *ndat = calloc (ncap, info->n_size);
-  if (ndat == NULL)
+  void *data = calloc (cap, info->n_size);
+  if (!data)
     return NULL;
 
-  hashmap nmap = { .len = 0, .cap = ncap, .data = ndat };
+  hashmap nmap = { .len = 0, .cap = cap, .data = data };
   for (size_t i = 0; i < map->cap; i++)
     {
       void *node = NODE_AT (map, i, info);
-      void *key = KEY_OF (node, info);
-      void *val = VAL_OF (node, info);
+      void *nkey = KEY_OF (node, info);
+      void *nval = VAL_OF (node, info);
 
       char sts = *STS_OF (node);
       if (sts == HASHMAP_STATE_EMPTY || sts == HASHMAP_STATE_RMED)
-        break;
+        continue;
 
       /* sts = HASHMAP_STATE_USED */
-      if (!hashmap_insert (&nmap, key, val, info))
+      if (!hashmap_insert (&nmap, nkey, nval, info))
         {
-          free (ndat);
+          free (data);
           return NULL;
         }
     }
 
   free (map->data);
-  map->data = ndat;
-  map->cap = ncap;
+  map->data = data;
+  map->cap = cap;
   return map;
 }
 
@@ -71,7 +65,7 @@ hashmap_find (hashmap *map, void *key, const hashmap_i *info)
     return NULL;
 
   size_t hash = info->f_hash (key);
-  size_t index = hash % map->cap;
+  size_t index = INDEX_AT (map, hash);
 
   for (size_t i = 1;; i++)
     {
@@ -83,23 +77,21 @@ hashmap_find (hashmap *map, void *key, const hashmap_i *info)
 
       if (sts == HASHMAP_STATE_RMED)
         {
-          index = NEXT_INDEX (index, i);
+          index = INDEX_NEXT (index, i);
           if (index < map->cap)
             continue;
           return NULL;
         }
 
+      /* sts = HASHMAP_STATE_USED */
       void *nkey = KEY_OF (node, info);
-      if (sts == HASHMAP_STATE_USED)
-        {
-          if (info->f_comp (key, nkey) == 0)
-            return node;
+      if (info->f_comp (key, nkey) == 0)
+        return node;
 
-          index = NEXT_INDEX (index, i);
-          if (index < map->cap)
-            continue;
-          return NULL;
-        }
+      index = INDEX_NEXT (index, i);
+      if (index < map->cap)
+        continue;
+      return NULL;
     }
 }
 
@@ -126,9 +118,9 @@ hashmap_insert (hashmap *map, void *key, void *val, const hashmap_i *info)
         return NULL;
     }
 
-  void *node;
+  void *node = NULL;
   size_t hash = info->f_hash (key);
-  size_t index = hash % map->cap;
+  size_t index = INDEX_AT (map, hash);
 
   for (size_t i = 1;; i++)
     {
@@ -138,27 +130,28 @@ hashmap_insert (hashmap *map, void *key, void *val, const hashmap_i *info)
       if (sts == HASHMAP_STATE_EMPTY || sts == HASHMAP_STATE_RMED)
         break;
 
+      /* sts = HASHMAP_STATE_USED */
       void *nkey = KEY_OF (node, info);
       if (info->f_comp (key, nkey) == 0)
         return NULL;
 
-      index = NEXT_INDEX (index, i);
+      index = INDEX_NEXT (index, i);
       if (index < map->cap)
         continue;
 
-      if (!hashmap_reserve (map, index + 1, info))
+      size_t ncap = map->cap * HASHMAP_EXPAN_RATIO;
+      if (!hashmap_reserve (map, ncap, info))
         return NULL;
     }
 
   void *nkey = KEY_OF (node, info);
   void *nval = VAL_OF (node, info);
-  *STS_OF (node) = HASHMAP_STATE_USED;
-
   if (memmove (nkey, key, info->k_size) != nkey)
     return NULL;
-
   if (memmove (nval, val, info->v_size) != nval)
     return NULL;
+
+  *STS_OF (node) = HASHMAP_STATE_USED;
 
   map->len++;
   return node;
