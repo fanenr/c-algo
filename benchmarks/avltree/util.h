17984ee02f0b13e60df72f6b0b26ab2cc6b84c1a
Arthur63 <Arthur63@163.com>
2024-03-23 22:43:20 +0800

    update experiments-avltree

--- benchmarks/avltree/util.h
+++ benchmarks/avltree/util.h
@@ -3,14 +3,14 @@
 
 #define attr_nonnull(...) __attribute__ ((nonnull (__VA_ARGS__)))
 
-#define is_same_t(a, b) __builtin_types_compatible_p (a, b)
-#define is_same_v(a, b) is_same_t (typeof (a), typeof (b))
+#define is_same_type(a, b)                                                    \
+  __builtin_types_compatible_p (typeof (a), typeof (b))
 
 #define container_of(ptr, type, member)                                       \
   ({                                                                          \
     void *mptr = (void *)(ptr);                                               \
-    static_assert (is_same_v (*(ptr), ((type *)0)->member)                    \
-                       || is_same_v (*(ptr), void),                           \
+    static_assert (is_same_type (*(ptr), ((type *)0)->member)                 \
+                       || is_same_type (*(ptr), void),                        \
                    "pointer type mismatch in container_of");                  \
     ((type *)(mptr - offsetof (type, member)));                               \
   })
