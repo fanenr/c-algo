f0f4797a35e0797c175d8381d363f45544e4ce03
Arthur63 <arthur63@163.com>
2024-03-28 13:03:02 +0800

    update list.c

--- list.h
+++ list.h
@@ -7,9 +7,6 @@
 typedef struct list_t list_t;
 typedef struct list_node_t list_node_t;
 
-typedef void list_dtor_t (list_node_t *n);
-typedef int list_comp_t (const list_node_t *a, const list_node_t *b);
-
 struct list_node_t
 {
   list_node_t *prev;
@@ -29,28 +26,21 @@ struct list_t
 extern list_node_t *list_at (const list_t *list, size_t index)
     attr_nonnull (1);
 
-extern list_node_t *list_push_back (list_t *list, list_node_t *node)
+extern void list_push_back (list_t *list, list_node_t *node)
     attr_nonnull (1, 2);
 
-extern list_node_t *list_push_front (list_t *list, list_node_t *node)
+extern void list_push_front (list_t *list, list_node_t *node)
     attr_nonnull (1, 2);
 
-extern list_node_t *list_insert_back (list_t *list, list_node_t *pos,
-                                      list_node_t *node)
-    attr_nonnull (1, 2, 3);
+extern void list_insert_back (list_t *list, list_node_t *pos,
+                              list_node_t *node) attr_nonnull (1, 2, 3);
 
-extern list_node_t *list_insert_front (list_t *list, list_node_t *pos,
-                                       list_node_t *node)
-    attr_nonnull (1, 2, 3);
+extern void list_insert_front (list_t *list, list_node_t *pos,
+                               list_node_t *node) attr_nonnull (1, 2, 3);
 
-extern list_node_t *list_insert_at (list_t *list, size_t index,
-                                    list_node_t *node) attr_nonnull (1, 3);
+extern void list_insert_at (list_t *list, size_t index, list_node_t *node)
+    attr_nonnull (1, 3);
 
 extern void list_erase (list_t *list, list_node_t *node) attr_nonnull (1, 2);
-
-extern list_node_t *list_find (const list_t *list, const list_node_t *target,
-                               list_comp_t *comp) attr_nonnull (1, 2, 3);
-
-extern void list_free (list_t *list, list_dtor_t *dtor) attr_nonnull (1, 2);
 
 #endif
