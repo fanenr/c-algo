e0e7479969eafc44c17e560eb41f5e38c017deb2
Arthur63 <arthur63@163.com>
2024-07-07 11:57:11 +0800

    add bitmap

---
+++ bitmap.c
@@ -0,0 +1,104 @@
+#include "bitmap.h"
+
+#define likely(exp) __builtin_expect (!!(exp), 1)
+#define unlikely(exp) __builtin_expect (!!(exp), 0)
+#define memset(src, c, n) __builtin_memset ((src), (c), (n))
+
+void
+bitmap_zero (bitmap_t *map)
+{
+  size_t size = BITMAP_BYTES (map->nbits);
+  memset (map->data, 0, size);
+  map->count = 0;
+}
+
+void
+bitmap_fill (bitmap_t *map)
+{
+  size_t size = BITMAP_BYTES (map->nbits);
+  memset (map->data, 0xff, size);
+  map->count = map->nbits;
+}
+
+void
+bitmap_set (bitmap_t *map, size_t pos)
+{
+  if (unlikely (pos >= map->nbits))
+    return;
+
+  size_t *s = map->data + pos / BITS_PER_ELEM;
+  size_t mask = (1UL << (pos % BITS_PER_ELEM));
+
+  if (!(*s & mask))
+    map->count++;
+
+  *s |= mask;
+}
+
+void
+bitmap_flip (bitmap_t *map, size_t pos)
+{
+  if (unlikely (pos >= map->nbits))
+    return;
+
+  size_t *s = map->data + pos / BITS_PER_ELEM;
+  size_t mask = (1UL << (pos % BITS_PER_ELEM));
+  *s ^= mask;
+
+  if (*s & mask)
+    map->count++;
+  else
+    map->count--;
+}
+
+void
+bitmap_clear (bitmap_t *map, size_t pos)
+{
+  if (unlikely (pos >= map->nbits))
+    return;
+
+  size_t *s = map->data + pos / BITS_PER_ELEM;
+  size_t mask = (1UL << (pos % BITS_PER_ELEM));
+
+  if (*s & mask)
+    map->count--;
+
+  *s &= ~mask;
+}
+
+int
+bitmap_at (const bitmap_t *map, size_t pos)
+{
+  if (unlikely (pos >= map->nbits))
+    return -1;
+
+  size_t *s = map->data + pos / BITS_PER_ELEM;
+  size_t mask = (1UL << (pos % BITS_PER_ELEM));
+  size_t v = *s & mask;
+
+  return !!v;
+}
+
+size_t
+bitmap_find_first0 (bitmap_t *map)
+{
+  size_t val, idx, elems = BITMAP_ELEMS (map->nbits);
+
+  for (idx = 0; idx < elems; idx++)
+    if ((val = map->data[idx]) != ~0UL)
+      return idx * BITS_PER_ELEM + __builtin_ffsl (~val) - 1;
+
+  return BITMAP_NONE;
+}
+
+size_t
+bitmap_find_first1 (bitmap_t *map)
+{
+  size_t val, idx, elems = BITMAP_ELEMS (map->nbits);
+
+  for (idx = 0; idx < elems; idx++)
+    if ((val = map->data[idx]) != 0UL)
+      return idx * BITS_PER_ELEM + __builtin_ffsl (val) - 1;
+
+  return BITMAP_NONE;
+}
---
+++ bitmap.h
@@ -0,0 +1,45 @@
+#ifndef BITMAP_H
+#define BITMAP_H
+
+#include <stddef.h>
+
+#define BITS_PER_BYTE 8
+#define BITS_PER_ELEM (sizeof (size_t) * BITS_PER_BYTE)
+
+#define BITMAP_ALIGN(x, a) (((x) + (a) - 1) & ~((a) - 1))
+#define BITMAP_ELEMS(n) (BITMAP_ALIGN (n, BITS_PER_ELEM) / BITS_PER_ELEM)
+#define BITMAP_BYTES(n) (BITMAP_ALIGN (n, BITS_PER_ELEM) / BITS_PER_BYTE)
+
+#define attr_nonnull(...) __attribute__ ((nonnull (__VA_ARGS__)))
+
+typedef struct bitmap_t bitmap_t;
+
+struct bitmap_t
+{
+  size_t *data;
+  size_t count;
+  size_t nbits;
+};
+
+#define BITMAP_NONE ((size_t) - 1)
+
+#define BITMAP_INIT                                                           \
+  (bitmap_t) {}
+
+void bitmap_zero (bitmap_t *map);
+void bitmap_fill (bitmap_t *map);
+
+void bitmap_set (bitmap_t *map, size_t pos);
+void bitmap_flip (bitmap_t *map, size_t pos);
+void bitmap_clear (bitmap_t *map, size_t pos);
+
+/* if @pos >= @map->nbits, -1 is returned. */
+int bitmap_at (const bitmap_t *map, size_t pos);
+
+/* if no 0-bits in @map, BITMAP_NONE is returned. */
+size_t bitmap_find_first0 (bitmap_t *map);
+
+/* if no 1-bits in @map, BITMAP_NONE is returned. */
+size_t bitmap_find_first1 (bitmap_t *map);
+
+#endif
